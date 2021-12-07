dafc298a3d37df290a72c2d57954736524e9d814
Michael Suhodolov <michaelsuhodolov@gmail.com>
2021-12-07 21:08:21 +0200

    audio

--- app/src/main/java/com/example/aihack/fragments/TestFragment.kt
+++ app/src/main/java/com/example/aihack/fragments/TestFragment.kt
@@ -1,13 +1,23 @@
 package com.example.aihack.fragments
 
+import android.Manifest
+import android.annotation.SuppressLint
 import android.content.Context
+import android.media.MediaPlayer
+import android.media.MediaRecorder
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.LinearLayout
+import androidx.core.app.ActivityCompat
+import androidx.core.content.PackageManagerCompat.LOG_TAG
 import androidx.fragment.app.Fragment
 import com.example.aihack.R
 import com.example.aihack.utils.CameraHelper
+import java.io.IOException
 
 class TestFragment : Fragment() {
     private lateinit var cameraHelper: CameraHelper
@@ -25,8 +35,20 @@ class TestFragment : Fragment() {
         return inflater.inflate(R.layout.test_fragment, container, false)
     }
 
+    @SuppressLint("CutPasteId")
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        fileName = "${requireActivity().externalCacheDir?.absolutePath}/audiorecordtest.3gp"
+        val button = view.findViewById<Button>(R.id.button)
+        val button2 = view.findViewById<Button>(R.id.button2)
+        button.setOnClickListener{
+            startRecording()
+        }
+        button2.setOnClickListener{
+            stopRecording()
+        }
+
+        ActivityCompat.requestPermissions(requireActivity(), permissions, REQUEST_RECORD_AUDIO_PERMISSION)
         cameraHelper = CameraHelper(
             owner = requireActivity(),
             context = safeContext,
@@ -49,7 +71,70 @@ class TestFragment : Fragment() {
         cameraHelper.onRequestPermissionsResult(requestCode)
     }
 
+    private var fileName: String = ""
+
+    private var recorder: MediaRecorder? = null
+
+
+    private var permissions: Array<String> = arrayOf(Manifest.permission.RECORD_AUDIO)
+
+//    override fun onRequestPermissionsResult(
+//        requestCode: Int,
+//        permissions: Array<String>,
+//        grantResults: IntArray
+//    ) {
+//        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+//        permissionToRecordAccepted = if (requestCode == REQUEST_RECORD_AUDIO_PERMISSION) {
+//            grantResults[0] == PackageManager.PERMISSION_GRANTED
+//        } else {
+//            false
+//        }
+//        if (!permissionToRecordAccepted) finish()
+//    }
+
+    private fun onRecord(start: Boolean) = if (start) {
+        startRecording()
+    } else {
+        stopRecording()
+    }
+
+    @SuppressLint("RestrictedApi")
+    private fun startRecording() {
+        recorder = MediaRecorder().apply {
+            setAudioSource(MediaRecorder.AudioSource.MIC)
+            setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP)
+            setOutputFile(fileName)
+            setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB)
+
+            try {
+                prepare()
+            } catch (e: IOException) {
+                Log.e(LOG_TAG, "prepare() failed")
+            }
+
+            start()
+            Log.d("START", "START")
+        }
+    }
+
+    private fun stopRecording() {
+        recorder?.apply {
+            stop()
+            release()
+        }
+        recorder = null
+        Log.d("STOP", "STOP")
+    }
+
+
+    override fun onStop() {
+        super.onStop()
+        recorder?.release()
+        recorder = null
+    }
+
     companion object {
         const val TAG = "CameraXDemo"
+        const val REQUEST_RECORD_AUDIO_PERMISSION = 200
     }
 }
