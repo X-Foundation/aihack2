76162497cfc0e928e7bd5e9a2364e55735691702
Michael Suhodolov <michaelsuhodolov@gmail.com>
2021-12-07 16:06:43 +0200

    fix

--- app/src/main/java/com/example/aihack/fragments/TestFragment.kt
+++ app/src/main/java/com/example/aihack/fragments/TestFragment.kt
@@ -1,18 +1,15 @@
 package com.example.aihack.fragments
 
-import android.content.pm.PackageManager
 import android.os.Bundle
 import android.util.Log
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.content.ContextCompat
-import com.example.aihack.utils.CameraHelper
+import androidx.fragment.app.Fragment
 import com.example.aihack.R
 import com.example.aihack.databinding.TestFragmentBinding
-import com.example.aihack.utils.CameraHelper.Companion.REQUIRED_PERMISSIONS
+import com.example.aihack.utils.CameraHelper
 
 class TestFragment : Fragment() {
     private lateinit var binding: TestFragmentBinding
