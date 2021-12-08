2f4c73c8074ad3491f309f6ffb781fd3c8d05685
Michael Suhodolov <michaelsuhodolov@gmail.com>
2021-12-08 16:13:18 +0200

    marking

--- app/src/main/java/com/example/aihack/fragments/TestFragment.kt
+++ app/src/main/java/com/example/aihack/fragments/TestFragment.kt
@@ -2,15 +2,22 @@ package com.example.aihack.fragments
 
 import android.annotation.SuppressLint
 import android.content.Context
+import android.graphics.Color
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import com.example.aihack.R
 import com.example.aihack.utils.CameraHelper
 import com.example.aihack.utils.VoiceHelper
+import android.text.style.ForegroundColorSpan
+
+import android.text.SpannableString
+
+import android.text.SpannableStringBuilder
 
 
 class TestFragment : Fragment() {
@@ -33,9 +40,29 @@ class TestFragment : Fragment() {
     @SuppressLint("CutPasteId")
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        val textView = view.findViewById<TextView>(R.id.textView2)
+        textView.text = SENTENCE
         val button = view.findViewById<Button>(R.id.button)
-        voiceHelper = VoiceHelper(requireActivity())
-        button.setOnClickListener{
+        voiceHelper = VoiceHelper(requireActivity(), onResult = {
+            val recognized = it.lowercase().replace('с', 'c').replace('c', 'с')
+            val split1 = recognized.split(' ')
+            val splitRight = SENTENCE.split(' ')
+            val builder = SpannableStringBuilder()
+            for ((idx, i) in splitRight.withIndex()) {
+                var text = i
+                if (idx != splitRight.size - 1)
+                    text = "$i "
+                val str1 = SpannableString(text)
+                if (i in split1) {
+                    str1.setSpan(ForegroundColorSpan(Color.GREEN), 0, str1.length, 0)
+                } else {
+                    str1.setSpan(ForegroundColorSpan(Color.RED), 0, str1.length, 0)
+                }
+                builder.append(str1)
+            }
+            textView.setText(builder, TextView.BufferType.SPANNABLE)
+        })
+        button.setOnClickListener {
             voiceHelper.startRecording()
         }
         cameraHelper = CameraHelper(
@@ -63,5 +90,6 @@ class TestFragment : Fragment() {
 
     companion object {
         const val TAG = "CameraXDemo"
+        const val SENTENCE = "какая сегодня хорошая погода"
     }
 }
