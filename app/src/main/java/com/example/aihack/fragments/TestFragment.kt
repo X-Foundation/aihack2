73b6da9db1386fdcf6c5447d3cc796f0393d44aa
Michael Suhodolov <michaelsuhodolov@gmail.com>
2021-12-08 08:28:00 +0200

    optimizations

--- app/src/main/java/com/example/aihack/fragments/TestFragment.kt
+++ app/src/main/java/com/example/aihack/fragments/TestFragment.kt
@@ -1,30 +1,22 @@
 package com.example.aihack.fragments
 
-import android.Manifest
 import android.annotation.SuppressLint
 import android.content.Context
-import android.content.Intent
 import android.os.Bundle
-import android.speech.RecognitionListener
-import android.speech.RecognizerIntent
-import android.speech.SpeechRecognizer
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
-import android.widget.Toast
-import androidx.core.app.ActivityCompat
 import androidx.fragment.app.Fragment
 import com.example.aihack.R
 import com.example.aihack.utils.CameraHelper
-import java.util.*
+import com.example.aihack.utils.VoiceHelper
 
 
 class TestFragment : Fragment() {
     private lateinit var cameraHelper: CameraHelper
+    private lateinit var voiceHelper: VoiceHelper
     private lateinit var safeContext: Context
-    private lateinit var speechRecognizer: SpeechRecognizer
-    private lateinit var speechRecognizerIntent: Intent
 
     override fun onAttach(context: Context) {
         super.onAttach(context)
@@ -42,15 +34,10 @@ class TestFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         val button = view.findViewById<Button>(R.id.button)
-        val button2 = view.findViewById<Button>(R.id.button2)
+        voiceHelper = VoiceHelper(requireActivity())
         button.setOnClickListener{
-            startRecording()
+            voiceHelper.startRecording()
         }
-        button2.setOnClickListener{
-            stopRecording()
-        }
-
-        ActivityCompat.requestPermissions(requireActivity(), permissions, REQUEST_RECORD_AUDIO_PERMISSION)
         cameraHelper = CameraHelper(
             owner = requireActivity(),
             context = safeContext,
@@ -58,24 +45,7 @@ class TestFragment : Fragment() {
             overlay = view.findViewById(R.id.graphicOverlay_finder)
         )
         cameraHelper.start()
-        speechRecognizer = SpeechRecognizer.createSpeechRecognizer(requireActivity())
-        speechRecognizerIntent = Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH)
-        speechRecognizerIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,RecognizerIntent.LANGUAGE_MODEL_FREE_FORM)
-        speechRecognizerIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale("ru_RU"))
-        speechRecognizer.setRecognitionListener(object : RecognitionListener {
-            override fun onReadyForSpeech(bundle: Bundle) {}
-            override fun onBeginningOfSpeech() {}
-            override fun onRmsChanged(v: Float) {}
-            override fun onBufferReceived(bytes: ByteArray) {}
-            override fun onEndOfSpeech() {}
-            override fun onError(i: Int) {}
-            override fun onResults(bundle: Bundle) {
-                val data = bundle.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION)
-                Toast.makeText(requireActivity(), data!![0], Toast.LENGTH_LONG).show()
-            }
-            override fun onPartialResults(bundle: Bundle) {}
-            override fun onEvent(i: Int, bundle: Bundle) {}
-        })
+
     }
 
     override fun onDestroy() {
@@ -90,19 +60,8 @@ class TestFragment : Fragment() {
     ) {
         cameraHelper.onRequestPermissionsResult(requestCode)
     }
-    private var permissions: Array<String> = arrayOf(Manifest.permission.RECORD_AUDIO)
-
-    @SuppressLint("RestrictedApi")
-    private fun startRecording() {
-        speechRecognizer.startListening(speechRecognizerIntent)
-    }
-
-    private fun stopRecording() {
-        speechRecognizer.stopListening()
-    }
 
     companion object {
         const val TAG = "CameraXDemo"
-        const val REQUEST_RECORD_AUDIO_PERMISSION = 200
     }
 }
---
+++ app/src/main/java/com/example/aihack/utils/VoiceHelper.kt
@@ -0,0 +1,60 @@
+package com.example.aihack.utils
+
+import android.Manifest
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.os.Bundle
+import android.speech.RecognitionListener
+import android.speech.RecognizerIntent
+import android.speech.SpeechRecognizer
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.fragment.app.FragmentActivity
+
+class VoiceHelper(
+    private val owner: FragmentActivity
+) {
+    private var speechRecognizer: SpeechRecognizer = SpeechRecognizer.createSpeechRecognizer(owner)
+    private var speechRecognizerIntent: Intent = Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH)
+
+    init {
+        ActivityCompat.requestPermissions(
+            owner, permissions,
+            REQUEST_RECORD_AUDIO_PERMISSION
+        )
+        speechRecognizerIntent.putExtra(
+            RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+            RecognizerIntent.LANGUAGE_MODEL_FREE_FORM
+        )
+        speechRecognizerIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, "ru_RU")
+        speechRecognizer.setRecognitionListener(object : RecognitionListener {
+            override fun onReadyForSpeech(bundle: Bundle) {}
+            override fun onBeginningOfSpeech() {}
+            override fun onRmsChanged(v: Float) {}
+            override fun onBufferReceived(bytes: ByteArray) {}
+            override fun onEndOfSpeech() {}
+            override fun onError(i: Int) {}
+            override fun onResults(bundle: Bundle) {
+                val data = bundle.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION)
+                Toast.makeText(owner, data!![0], Toast.LENGTH_LONG).show()
+            }
+
+            override fun onPartialResults(bundle: Bundle) {}
+            override fun onEvent(i: Int, bundle: Bundle) {}
+        })
+    }
+
+    @SuppressLint("RestrictedApi")
+    fun startRecording() {
+        speechRecognizer.startListening(speechRecognizerIntent)
+    }
+
+    fun stopRecording() {
+        speechRecognizer.stopListening()
+    }
+
+    companion object {
+        private var permissions: Array<String> = arrayOf(Manifest.permission.RECORD_AUDIO)
+        const val REQUEST_RECORD_AUDIO_PERMISSION = 200
+    }
+}
